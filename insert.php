42ce54aa4b8ec58687ba723a9f91e783617573c4
Anna2Bdx <124666087+Anna2Bdx@users.noreply.github.com>
2024-01-11 21:50:26 +0100

    correction script insert

--- insert.php
+++ insert.php
@@ -47,13 +47,16 @@
     
         if (mysqli_num_rows($res) <= 0) {
             //Create table if it does not exist
-            
             //print("la table n'existe pas, on demande la création.")
             $sql = file_get_contents("database/schema_final.sql");
             if(!mysqli_query($conn, $sql)){
-                die('Table Creation Failed');
+                die('Table températures Failed');
                 //print('la création de la table a échoué');
+            } else {
+                die('Table créée');
             }
+        } else {
+            die('Table existante');
         }
 
         $res = mysqli_query($conn, "INSERT INTO Temperatures (dateYMD, temperature, humidity) VALUES ('20240111', '26.4', '42.0')");
@@ -66,7 +69,7 @@
         }
         
             
-        }
+        
 
         //Close the connection
         mysqli_close($conn);
