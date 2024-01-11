2e5d2579cee5fa7b5ccd7b520aa87d4afcdf0a45
Anna2Bdx <124666087+Anna2Bdx@users.noreply.github.com>
2024-01-11 19:01:07 +0100

    correction de la création de la table du capteur

--- insert.php
+++ insert.php
@@ -41,6 +41,17 @@
             
         }
 
+
+        $res = mysqli_query($conn, "SHOW TABLES LIKE 'Temperatures'");
+    
+        if (mysqli_num_rows($res) <= 0) {
+            //Create table if it does not exist
+            $sql = file_get_contents("database/schema.sql");
+            if(!mysqli_query($conn, $sql)){
+                die('Table Creation Failed');
+            }
+        }
+
         if ($stmt = mysqli_prepare($conn, "INSERT INTO Temperatures (dateYMD, temperatures, humidity) VALUES (?, ?)")) {
             mysqli_stmt_bind_param($stmt, 'sd', "20240111", 26, 42);
             mysqli_stmt_execute($stmt);
