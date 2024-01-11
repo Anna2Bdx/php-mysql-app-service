a195cc53d5408f048d49ac09c21406777e0d0ba3
Anna2Bdx <124666087+Anna2Bdx@users.noreply.github.com>
2024-01-11 22:44:47 +0100

    on ajoute la fonction de lecture des données en base

--- lire.php
+++ lire.php
@@ -10,24 +10,26 @@
         $conn = mysqli_init();
         mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
         if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
-            die('Failed to connect to MySQL: '.mysqli_connect_error());
+            //die('Failed to connect to MySQL: '.mysqli_connect_error());
+            echo("on n'a pas réussi à se connecter à la base");
         }
         $query = "SELECT * FROM Temperatures";
         $response = array();
+        echo("on balance la requête");
         $result = mysqli_query($conn, $query);
         while($row = mysqli_fetch_array($result))
         {
             $response[] = $row;
         }
-        header('Content-Type: application/json');
-        echo json_encode($response, JSON_PRETTY_PRINT);
+        //header('Content-Type: application/json');
+        //echo json_encode($response, JSON_PRETTY_PRINT);
     }
 
     switch($request_method)
     {
         case 'GET':
             echo ("on a bien reconnu la demande de GET");
-            //getTemp();
+            getTemp();
             break;
         default:
             // Requête invalide
