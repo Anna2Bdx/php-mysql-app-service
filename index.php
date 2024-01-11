8da291caeadc17950431df79433860182c411386
Anna2Bdx <124666087+Anna2Bdx@users.noreply.github.com>
2024-01-11 22:05:59 +0100

    test d'une API

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
 	<ul>
 		<div class="left-align">
 			<li><a href="read.php"><strong>Voir le catalogue</strong></a></li>
-			<li><a href="insert.php"><strong>Add a Product</strong></a></li>
-			<li><a href="update.php"><strong>Update a Product</strong></a></li>
-			<li><a href="delete.php"><strong>Remove a Product</strong></a></li>
+			<li><a href="insert.php"><strong>Ajouter un produit</strong></a></li>
+			<li><a href="update.php"><strong>Modifier un produit</strong></a></li>
+			<li><a href="delete.php"><strong>Supprimer un produit</strong></a></li>
+			<li><a href="insertAPI.php"><strong>Ajouter une température</strong></a></li>
 		</div>
 	</ul>
 </div>
