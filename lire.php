9f222b03fd18c415212516ad18eb22b50eede72c
Anna2Bdx <124666087+Anna2Bdx@users.noreply.github.com>
2024-01-11 23:06:48 +0100

    ajout de la méthode POST

--- lire.php
+++ lire.php
@@ -26,11 +26,48 @@
         echo json_encode($response, JSON_PRETTY_PRINT);
     }
 
+    function addTemp()
+	{
+		require "database/config.php";
+        //echo ("on est entré dans la fonction");
+        //Establish the connection
+        $conn = mysqli_init();
+        mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
+        if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
+            die('Failed to connect to MySQL: '.mysqli_connect_error());
+        }
+		$date = $_POST["dateYMD"];
+		$temp = $_POST["temp"];
+		$humi = $_POST["humi"];
+		
+		echo $query="INSERT INTO Temperatures( dateYMD, temperature, humidity) VALUES('".$date."', '".$temp."', '".$humi."')";
+		if(mysqli_query($conn, $query))
+		{
+			$response=array(
+				'status' => 1,
+				'status_message' =>'Donnée ajoutée avec succès.'
+			);
+		}
+		else
+		{
+			$response=array(
+				'status' => 0,
+				'status_message' =>'ERREUR!.'. mysqli_error($conn)
+			);
+		}
+		header('Content-Type: application/json');
+		echo json_encode($response);
+	}
+
     switch($request_method)
     {
         case 'GET':
             //echo ("on a bien reconnu la demande de GET");
             getTemp();
+            break;
+        case 'POST':
+            //echo ("on a bien reconnu la demande de GET");
+            addTemp();
             break;
         default:
             // Requête invalide
