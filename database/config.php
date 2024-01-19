424115081643de0165d983249da5135847e5dbbb
Anna2Bdx <124666087+Anna2Bdx@users.noreply.github.com>
2024-01-19 19:11:51 +0100

    changement de clé d'authentification

--- database/config.php
+++ database/config.php
@@ -7,4 +7,4 @@ $username   = getenv('DB_USERNAME');
 $password   = getenv('DB_PASSWORD');
 $db_name     = getenv('DB_DATABASE');
 $sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
-$auth_key   = "123";
+$auth_key   = "Zx65IRZhxX8p3u2z07aBEb";
